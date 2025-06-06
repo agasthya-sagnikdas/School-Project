71c404e1b6cfdad1b6da741cb37825b473449703
agasthya-sagnikdas <agasthyaelectrocomputing@gmail.com>
2025-06-06 11:47:48 +0530

    Program 4

---
+++ 04-simpleinterest.py
@@ -0,0 +1,8 @@
+def interest(p1,r1,t1 ):
+ i=p*r*t/100
+ return(i)
+p=int(input("Enter principle="))
+r=int(input("Enter rate="))
+t=int(input("Enter rate="))
+in1=interest(p,r,t)
+print("Interest=",in1)
